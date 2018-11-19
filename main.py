e3c2ec2887c4753068af69eb1aada8aa6e03b2da
Bernardo Covas <bernardo.covas@outlook.com>
2018-11-19 08:02:17 +0000

    Added __init__.py to ./package root. random (mostly useless) changes.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import logging
 
 from package.dataset import Dataset
-
+from package.common import PathsJson
 
 def main():
     dataset = Dataset()
     dataset.prepare()
+    config = PathsJson()
+    
 
 
 if __name__ == "__main__":
