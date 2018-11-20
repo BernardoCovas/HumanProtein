8a1ef6e091554addc105ed50ff53d79babd29021
Bernardo Covas <bernardo.covas@outlook.com>
2018-11-20 04:12:15 +0000

    added command line args and saved model

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import logging
+import datetime
 
 import tensorflow as tf
 
@@ -17,17 +18,13 @@ def _model_fn(feature_dataset: tf.data.Dataset, label_dataset: tf.data.Dataset):
 
     _, loss = model.predict_train(feature_batch, label_batch)
 
-    return loss
+    return loss, model.input_tensor, model.output_tensor
 
-def train(epochs: int, batch_size: int):
+def train(epochs: int, batch_size: int, prefetch: bool):
 
     logger = logging.getLogger("trainer")
     config = ConfigurationJson()
-
-    if epochs is None:
-        epochs = config.num_epochs
-    if batch_size is None:
-        batch_size = config.batch_size
+    paths = PathsJson()
 
     logger.info(f"Using batch of {batch_size}")
     logger.info(f"Training for {epochs} epochs")
@@ -45,11 +42,10 @@ def train(epochs: int, batch_size: int):
         feature_dataset = feature_dataset.batch(batch_size)
         label_dataset = label_dataset.batch(batch_size)
 
+        # feature_dataset = feature_dataset.apply(tf.data.experimental.prefetch_to_device("gpu:0", batch_size))
+        # label_dataset   = label_dataset.apply(tf.data.experimental.prefetch_to_device("gpu:0", batch_size))
 
-        feature_dataset = feature_dataset.apply(tf.data.experimental.prefetch_to_device("gpu:0", batch_size))
-        label_dataset   = label_dataset.apply(tf.data.experimental.prefetch_to_device("gpu:0", batch_size))
-
-        loss_tensor = _model_fn(feature_dataset, label_dataset)
+        loss_tensor, model_inp_tensor, model_outp_tensor = _model_fn(feature_dataset, label_dataset)
         optimize_op = tf.train.AdamOptimizer().minimize(loss_tensor)
 
         with tf.Session() as sess:
@@ -61,13 +57,52 @@ def train(epochs: int, batch_size: int):
                 if i % 100 == 0:
                     logger.info(f"Step: {i} of {epochs}, Loss: {loss}")
 
+            logger.info(f"Finished training. Saving model to {paths.MODEL_CHECKPOINT_DIR}")
+            tf.saved_model.simple_save(
+                sess,
+                paths.MODEL_CHECKPOINT_DIR,
+                inputs={"input": model_inp_tensor},
+                outputs={"output": model_outp_tensor})
 
 
 if __name__ == "__main__":
 
+    import argparse
     import os
-    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+    import shutil
 
     logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger("train_script")
+    config = ConfigurationJson()
+    paths = PathsJson()
 
-    train(None, None)
+    tf.logging.set_verbosity(tf.logging.ERROR)
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+    parser = argparse.ArgumentParser(
+        description="Parse the dataset for efficient training.")
+
+    parser.add_argument("--prefetch_gpu", action="store_true", help="""
+Prefetch to gpu for improved performance. Saved model might not work on cpu-only machines.
+""")
+    parser.add_argument("--epochs", type=int, default=config.num_epochs, help=f"""
+The number of training steps. Defaults to the config file value ({config.num_epochs}).
+""")
+    parser.add_argument("--batch_size", type=int, default=config.batch_size, help=f"""
+The number of images that go through the deep learing model at once. 
+Large numbers can improve model quality. Defaults to the config file value ({config.batch_size}).
+""")
+    parser.add_argument("--overwrite", action="store_true", help="""
+Overwrite an existing saved_model directory.
+""")
+
+    args = parser.parse_args()
+
+    if os.path.exists(paths.MODEL_CHECKPOINT_DIR):
+        if args.overwrite:
+            shutil.rmtree(paths.MODEL_CHECKPOINT_DIR)
+        else:
+            logger.error("Checkpoint dir not empty. Use '-h' for options.")
+            exit()
+
+    train(args.epochs, args.batch_size, args.prefetch_gpu)
