bb1232d44827af88841c9574d4b7afd3d94c2c21
Bernardo Covas <bernardo.covas@outlook.com>
2018-11-21 03:26:15 +0000

    Completely changed dataset class. Added a labeling function. Aded a new function returning a tf.data.Dataset suitable for image and filename batching. Now everything happens inside the computation graph.

--- package/dataset.py
+++ package/dataset.py
@@ -22,26 +22,28 @@ class Dataset:
     `https://www.kaggle.com/c/human-protein-atlas-image-classification`
     """
 
-    _logger = logging.getLogger("dataset_class")
+    _logger = logging.getLogger("DatasetClass")
     _filter_list = ["red", "green", "blue", "yellow"]
     # NOTE (bcovas) Almost for sure we do not need the yellow filter.
 
     dataset_path = ".data"
-    _train_img_ids = {}
+    _img_format = "png"
+    _train_id_data = {}
 
     _all_zip = "all.zip"
     _train_zip = "train.zip"
     _test_zip = "test.zip"
-    _csv_file = "train.csv"
-    _csv_sample = "sample_submission.csv"
+    _train_csv = "train.csv"
+    _sample_csv = "sample_submission.csv"
     _train_dir ="train"
     _test_dir = "test"
 
-    _all_contents = [_train_dir, _test_dir, _csv_file, _csv_sample]
+    _all_contents = [_train_dir, _test_dir, _train_csv, _sample_csv]
+    _img_filters = ["red", "green", "blue", "yellow"]
 
     def __init__(self):
-        config = common.PathsJson()
-        self.dataset_path = config.RAW_DATA_DIR
+        self.dataset_path = common.PathsJson().RAW_DATA_DIR
+        self._load_train_data()
 
     @property
     def train_dir(self):
@@ -50,44 +52,28 @@ class Dataset:
     @property
     def test_dir(self):
         return self._raw_path(self._test_dir)
-
+    
     @property
-    def train_ids(self):
-
-        n_imgs = len(self._train_img_ids.keys())
-        if n_imgs == 0:
-            raise IndexError(
-                "Dataset not prepared. Don't forget to call self.prepare().")
-
-        return self._train_img_ids
-
-    def get_id_paths(self, img_id: str):
-
-        if img_id not in self._train_img_ids.keys():
-            raise IndexError("Not a valid id: " + img_id)
-
-        data = {}
-        for color_filter in self._filter_list:
-
-            img_name = f"{img_id}_{color_filter}.png"
-            img_name = os.path.join(self.dataset_path, self._train_dir, img_name)
-
-            if not os.path.exists(img_name):
-                raise FileNotFoundError(f"id {img_id} exists, but it's " + \
-                    f"{color_filter} filter does not seem to exist.")
-
-            data[color_filter] = img_name
-
-        data["labels"] = self._train_img_ids.get(img_id)
-        self._logger.debug("Found: " + img_id)
-
-        return data
+    def num_examples(self):
+        return len(self._train_id_data.keys())
 
     def prepare(self):
 
         if self._prepared():
             self._logger.info("Already extracted. Skipping.")
             return
+        
+        if not common.ConfigurationJson().OVERWRITE_DATASET_IF_CURRUPTED:
+            self._logger.error("Dataset currupted.")
+            exit(1)
+
+        self._logger.info("Dataset currupted. Deleting...")
+
+        # FIXME
+        exit()
+
+        shutil.rmtree(self._raw_path(self._train_dir), ignore_errors=True)
+        shutil.rmtree(self._raw_path(self._test_dir), ignore_errors=True)
 
         _train_zip = self._raw_path(self._train_zip)
         _test_zip = self._raw_path(self._test_zip)
@@ -112,6 +98,28 @@ class Dataset:
 
         self._logger.info("Done.")
 
+
+    def label(self, img_id: str):
+        return self._train_id_data.get(img_id)
+
+    def _load_train_data(self):
+        
+        # NOTE (bcovas) If already loaded
+        if self._train_id_data != {}:
+            return
+
+        csv = self._raw_path(self._train_csv)
+        with open(csv) as f:
+
+            # NOTE (bcovas) Skipping header
+            f.__next__()
+
+            for row in f:
+                row = row.replace("\n", "")
+                img_id, label = row.split(",")
+                img_labels = label.split(" ")
+                self._train_id_data[img_id] = img_labels
+
     def _raw_path(self, file: str):
         return os.path.join(self.dataset_path, file)
 
@@ -129,8 +137,6 @@ class Dataset:
         self._logger.info(f"Done: {filename}")
 
     def _prepared(self):
-        
-        import csv
 
         for _file in self._all_contents:
 
@@ -139,19 +145,14 @@ class Dataset:
                 return False
 
         existing_imglist = os.listdir(self.train_dir)
-        imglist = []
+        img_list = []
 
-        with open(self._raw_path(self._csv_file)) as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                
-                _id = row.get("Id")
-                self._train_img_ids[_id] = row.get("Target").split(" ")
+        for img_id in self._train_id_data.keys():
+            for img_filter in self._img_filters:
+                fname = f"{img_id}_{img_filter}.{self._img_format}"
+                img_list.append(fname)
 
-                for img_filter in self._filter_list:
-                    imglist.append(_id + f"_{img_filter}" + ".png")
-
-        return sorted(imglist) == sorted(existing_imglist)
+        return sorted(img_list) == sorted(existing_imglist)
 
 # TENSORFLOW FUNCTIONS
 
@@ -169,36 +170,74 @@ def _float_feature(value):
     return tf.train.Feature(float_list=tf.train.FloatList(value=value))
 
 def _bytes_feature(value):
-  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+    if type(value) == str:
+        value = value.encode()
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
-def tf_write_single_example(image: bytes, img_features: np.ndarray, labels: [], img_id: str):
+def tf_write_single_example(img_features: np.ndarray, labels: [], img_id: str, image_bytes=None):
 
-    if type(img_id) == str:
-        img_id = img_id.encode()
+    feature = {
+        TFRecordKeys.LABEL_KEY: _int64_feature(labels),
+        TFRecordKeys.ID_KEY: _bytes_feature(img_id),
+        TFRecordKeys.IMG_FEATURES: _float_feature(img_features)
+    }
 
-    feature = {TFRecordKeys.LABEL_KEY: _int64_feature(labels),
-               TFRecordKeys.ID_KEY: _bytes_feature(img_id),
-               TFRecordKeys.IMG_FEATURES: _float_feature(img_features)}
-
-    if image is not None:
-        feature[TFRecordKeys.ENCODED_KEY] = _bytes_feature(image),
+    if image_bytes is not None:
+        feature[TFRecordKeys.ENCODED_KEY] = _bytes_feature(image_bytes),
 
     example = tf.train.Example(features=tf.train.Features(feature=feature))
     return example.SerializeToString()
 
-def tf_parse_single_example(serialized_example: bytes, load_images=False):
+def tf_parse_single_example(serialized_example: bytes, load_image_bytes=False):
 
     feature = {
-        TFRecordKeys.ID_KEY: tf.FixedLenFeature([], tf.string),
+        TFRecordKeys.IMG_FEATURES: tf.FixedLenSequenceFeature([], tf.float32,
+            allow_missing=True),
         TFRecordKeys.LABEL_KEY: tf.FixedLenSequenceFeature([], tf.int64,
             allow_missing=True),
-        TFRecordKeys.IMG_FEATURES: tf.FixedLenSequenceFeature([], tf.float32,
-            allow_missing=True)
+        TFRecordKeys.ID_KEY: tf.FixedLenFeature([], tf.string)
     }
 
-    if load_images:
+    if load_image_bytes:
         feature[TFRecordKeys.ENCODED_KEY] = tf.FixedLenFeature([], tf.string)
 
     features = tf.parse_single_example(serialized_example, features=feature)
-
     return features
+
+def tf_preprocess_directory_dataset(dirname: str, paralell_readers=None):
+        """
+        Retruns a tf.data.Dataset that iterates `dirname` and yields (image_tensor, img_id).
+        """
+        
+        def _map_fn(*channels):
+
+            image_tensor = []
+
+            for channel in channels:
+                channel = tf.image.decode_image(channel)
+                channel = tf.squeeze(channel)
+                
+                image_tensor.append(channel)
+
+            image_tensor = tf.stack(image_tensor, -1)
+
+            return image_tensor
+        
+        filters = ["red", "green", "blue"]
+        datasets = []
+        file_name_datasets = []
+
+        for filter_name in filters:
+            dataset = tf.data.Dataset.list_files(
+                # NOTE (bcovas) Match all files of a single filter
+                os.path.join(dirname, f"*{filter_name}.png"), shuffle=False)
+            file_name_datasets.append(dataset)
+            dataset = dataset.map(lambda x: tf.read_file(x), paralell_readers)
+            datasets.append(dataset)
+
+        file_name_dataset = tf.data.Dataset.zip(tuple(file_name_datasets)) \
+            .map(lambda *fnames: tf.stack(fnames))
+        
+        dataset = tf.data.Dataset.zip(tuple(datasets)).map(_map_fn, paralell_readers)
+
+        return tf.data.Dataset.zip((dataset, file_name_dataset))
