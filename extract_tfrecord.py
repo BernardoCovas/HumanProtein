b15ec9fe61a309d5a5c611a77f4ccf2406ac5d48
Bernardo Covas <bernardo.covas@outlook.com>
2018-11-26 18:13:43 +0000

    added image path

--- extract_tfrecord.py
+++ extract_tfrecord.py
@@ -27,17 +27,19 @@ def main(tfrecord_path: str):
         dataset = dataset.map(lambda x: dataset_module.tf_parse_single_example(
             x, keys=[
                 dataset_module.TFRecordKeys.ID_KEY, 
-                dataset_module.TFRecordKeys.LABEL_KEY]), 2)
+                dataset_module.TFRecordKeys.LABEL_KEY,
+                dataset_module.TFRecordKeys.IMG_PATHS_KEY]), 2)
         dataset = dataset.prefetch(10)
 
-        img_id_tensor, label_tensor = dataset.make_one_shot_iterator().get_next()
+        img_id_tensor, label_tensor, img_paths_tensor = dataset.make_one_shot_iterator().get_next()
 
         sess = tf.Session()
         n = 0
         while True:
             try:
-                img_id, labels = sess.run([img_id_tensor, label_tensor])
+                img_id, labels, paths = sess.run([img_id_tensor, label_tensor, img_paths_tensor])
                 print(f"{img_id.decode()} -> {str(labels)}")
+                [print(p) for p in paths]
                 n += 1
             except tf.errors.OutOfRangeError:
                 break
